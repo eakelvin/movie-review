5b98180a8554d57db43f272ad356a5ef296b1265
Kelvin Akaba <akabakelvin@gmail.com>
2023-06-05 23:58:05 +0000

    movie review

---
+++ src/assets/Review.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+class Review extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            reviews : []
+        }
+
+    }
+
+    componentDidMount() {
+        fetch("https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=3AnqdqcAh9BPx7gOJCbX7qsohsT8hRjO")
+        .then(response => response.json())
+        .then((data) => {
+            this.setState({reviews : data.results})
+            console.log(data);
+        })
+        .catch((error) => console.log(error))
+    }
+    
+
+  render() {
+    const {reviews} = this.state
+
+    return (
+      <div className='container'>
+        {
+            reviews.map((movie, index) => (
+                <div key={index} className='box'>
+                    <h3>Title :- {movie.display_title}</h3>
+                    <p>Headline :- {movie.headline}</p>
+                    <p>Critics Pick :- {movie.criticpick}</p>
+                    <p>Byline :- {movie.byline}</p>
+                </div>
+            ))
+        }
+      </div>
+    )
+  }
+}
+
+export default Review
